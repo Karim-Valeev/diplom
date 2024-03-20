867dffeb7878e66f0854d3678c07e050d774e9d8
Karim-Valeev <46091087+Karim-Valeev@users.noreply.github.com>
2024-03-20 11:41:37 +0300

    Solve all TODO's (#8)
    * Add main page
    
    * Add login, registration, profile views
    
    * Add mixin
    
    * Add video views and celery
    
    * Add delete video view
    
    * Add blank views
    
    * Add download logic
    
    * Add ML files
    
    * Add recognition status display; Fix flower
    
    * Fix status display
    
    * Add recognition model creation
    
    * Add recognition output capture
    
    * Add statistics drawing
    
    * Add download feature

--- web/app/logic.py
+++ web/app/logic.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
 from celery.result import AsyncResult
-from django.http import FileResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404
 
 from app.models import Recognition, Video
 from app.tasks import recognize_actions
@@ -12,10 +13,11 @@ class RecognitionStatuses(Enum):
     Depending on the name of the status, it returns its translation and a flag
     for the possibility of starting the next recognition.
     """
-    PENDING = 'Задача прервалась, попробуйте еще раз.', True
-    STARTED = 'В процессе...', False
+    PENDING = 'Задача в ожидании запуска или утеряна, можете попробовать еще раз.', True
+    STARTED = 'Задача запустилась.', False
+    RECOGNIZING = 'Идет распознавание...', False
     SUCCESS = 'Завершилось!', True
-    FAILURE = 'Возникла ошибка.', False
+    FAILURE = 'Возникла ошибка. Свяжитесь с администратором.', False
 
 
 def start_recognition(video_id: int):
@@ -24,7 +26,6 @@ def start_recognition(video_id: int):
 
 
 def get_recognition_task_status(video: Video) -> tuple[str | None, bool]:
-    # TODO: Try to optimize query with video join
     recognition = video.recognitions.order_by('created').last()
     if recognition:
         task_id = recognition.task_id
@@ -35,27 +36,33 @@ def get_recognition_task_status(video: Video) -> tuple[str | None, bool]:
 
 
 def get_recognized_video_response(recognition_id: int):
-    recognition = Recognition.objects.filter(id=recognition_id).first()
-    if recognition:
-        recognized_file = recognition.recognized_file
-        path = recognized_file.path
-        name = recognized_file.name
-        with open(path, 'rb') as file:
-            response = FileResponse(
-                file.read(),
-                content_type='application/force-download'
-            )
-            response['Content-Disposition'] = f'attachment; filename="{name}"'
-            return response
-    else:
-        # TODO: Raise 404 exception
-        pass
+    recognition = get_object_or_404(Recognition, id=recognition_id)
+    recognized_file = recognition.recognized_file
+    path = recognized_file.path
+    name = recognized_file.name.split('/')[-1]
+    with open(path, 'rb') as file:
+        response = HttpResponse(
+            file.read(),
+            content_type='application/force-download'
+        )
+        response['Content-Disposition'] = f'attachment; filename="{name}"'
+        return response
 
 
-def get_recognition_stats_response(recognition_id: int):
-    recognition = Recognition.objects.filter(id=recognition_id).first()
-    if recognition:
-        raise NotImplementedError
+def get_recognition_stats_response(recognition_id: int, stat_type: str | None):
+    recognition = get_object_or_404(Recognition, id=recognition_id)
+    if stat_type == 'graph':
+        stat_file = recognition.stat_graph
+    elif stat_type == 'pie_chart':
+        stat_file = recognition.stat_pie_chart
     else:
-        # TODO: Raise 404 exception
-        pass
+        return HttpResponseBadRequest
+    path = stat_file.path
+    name = stat_file.name.split('/')[-1]
+    with open(path, 'rb') as file:
+        response = HttpResponse(
+            file.read(),
+            content_type='application/force-download'
+        )
+        response['Content-Disposition'] = f'attachment; filename="{name}"'
+        return response
